756cf88b2faa7830c3cf9b6209fdaabdf91c2ef8
HARITHA V R <166034924+harithavr99@users.noreply.github.com>
2024-12-04 14:39:02 +0530

    Add files via upload
    Health and Fitness Tracking Management System
    A comprehensive system for managing health and fitness goals. Users can track personal fitness data, set goals, and monitor progress through interactive dashboards. Features include:
    
    User Management: Profiles for users and administrators.
    Fitness Tracking: Input and monitor activity levels, weight, height, and step counts.
    Goal Setting: Define fitness objectives, such as weight loss or muscle gain.
    Notifications: Timely reminders for workouts, meals, hydration, and health alerts.
    Preferences: Customize themes, units, and language settings.
    Integration: Connect with wearables, health apps, and enable social media sharing.
    Built using React for the frontend, ASP.NET Core for the backend, and Entity Framework for database management.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+
+using Final_react.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Final_react
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<FitnessDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Reactfit")));
+
+
+            //configure cors
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowReactApp",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:5173")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+            });
+
+
+
+            var app = builder.Build();
+
+            //use cors
+            app.UseCors("AllowReactApp");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
